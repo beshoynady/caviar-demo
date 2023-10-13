782aa8fc1e0a1983d32cdcb6c236cb20aab33796
beshoynady <bishoin2017@gmail.com>
2023-10-14 01:27:35 +0300

    Kitchen

--- client/src/screens/management/manag.component/kitchen/Kitchen.jsx
+++ client/src/screens/management/manag.component/kitchen/Kitchen.jsx
@@ -26,21 +26,6 @@ const Kitchen = () => {
     }
   }
 
-  // const [waiters, setwaiters] = useState([])
-  // const getAllWaiter = async()=>{
-  //   const alluser = await axios.get('https://caviar-api.vercel.app/api/user')
-  //   const allwaiter =await alluser.data.filter((user)=>user.role === 'waiter')
-  //   // console.log(allwaiter)
-  //   const listId = []
-  //   allwaiter.forEach((waiter)=>{
-  //     listId.push(waiter._id)
-  //   })
-  //   // console.log(listId)
-  //   if(listId.length>0){
-  //     setwaiters(listId)
-  //   }
-  // }
-
   const [waiters, setwaiters] = useState([])
   const getAllWaiter = async () => {
     const alluser = await axios.get('https://caviar-api.vercel.app/api/user')
@@ -60,26 +45,7 @@ const Kitchen = () => {
     }
   }
 
-
   // const [waiter, setwaiter] = useState()
-  // const specifiedWaiter =()=>{
-  //   const lastwaiter = allorders? allorders[allorders.length-1].waiter:''
-  //   console.log(lastwaiter)
-  //   const indexoflastwaiter = waiters.indexOf(lastwaiter)
-
-  //   console.log(indexoflastwaiter)
-  //   console.log(indexoflastwaiter+1)
-  //   console.log(waiters.length)
-  //   console.log(waiters)
-  //   // setwaiter(waiters[indexofwaiter+1])
-  //   if(waiters.length < indexoflastwaiter+1){
-  //     const waiter = waiters[0]
-  //     return waiter     
-  //   }else{
-  //     const waiter = waiters[indexoflastwaiter+1]
-  //     return waiter
-  //   }
-  // }
   const specifiedWaiter = () => {
     const ordertakewaiter = allorders.filter((order)=> order.waiter != null)
     console.log(ordertakewaiter)
@@ -101,10 +67,9 @@ const Kitchen = () => {
       return waiter
     }
   }
-
   const orderInprogress = async (id) => {
     // await specifiedWaiter();
-    const waiter = specifiedWaiter()
+    const waiter =await specifiedWaiter()
     const status = 'جاري التحضير'
     const order = await axios.put('https://caviar-api.vercel.app/api/order/' + id, {
       status,waiter

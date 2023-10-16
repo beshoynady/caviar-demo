1fa98ffa064c94dd5c238c825a3993e20940513b
beshoynady <bishoin2017@gmail.com>
2023-10-16 17:56:37 +0300

    producr

--- server/router/Product.router.js
+++ server/router/Product.router.js
@@ -16,7 +16,7 @@ const multer = require("multer");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./images");
+    cb(null, "images/");
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = new Date().toISOString().replace(/:/g, "-");

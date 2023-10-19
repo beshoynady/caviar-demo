4ccffbf0f96c81667d899733e66ed66fc2cb48d3
beshoynady <bishoin2017@gmail.com>
2023-10-19 12:59:49 +0300

    pos invoice

--- client/src/screens/management/manag.component/pos/POS.jsx
+++ client/src/screens/management/manag.component/pos/POS.jsx
@@ -102,78 +102,70 @@ const POS = () => {
 
                         <div className="invoice side" >
                           <div ref={printContainer} className="side-content">
-                            <table className="invoice-info-container">
-                              <tbody>
-                                <tr>
-                                  <td rowSpan="2" className="client-name">
-                                    عميل:{userlogininfo ? usertitle(userlogininfo.id) : ''}
-                                  </td>
-                                  <td rowSpan="2">
-                                    كافيار
-                                  </td>
-                                </tr>
-                                <tr>
-                                  <td>
-                                    Invoice Date: <strong>{orderupdate_date}</strong>
-                                  </td>
-                                  <td>
-                                    Invoice No: <strong>{myorder.serial}</strong>
-                                  </td>
-                                </tr>
-                              </tbody>
-                            </table>
-                            <table className="line-items-container">
-                              <thead>
-                                <tr>
-                                  <th className="bold heading-name">المنتج</th>
-                                  <th className="bold heading-quantity">الكمية</th>
-                                  <th className="bold heading-price">السعر</th>
-                                  <th className="bold heading-subtotal">التكلفه</th>
-                                </tr>
-                              </thead>
-                              <tbody>
-                                {list_produccts_order.map((item, i) => {
-                                  return (
-                                    <tr key={i}>
-                                      <td className="bold heading-name">{item.name}</td>
-                                      <td className="bold heading-quantity">{item.quantity}</td>
-                                      <td className="bold heading-price">{item.price}</td>
-                                      <td className="bold heading-subtotal">{item.totalprice}</td>
-                                    </tr>
-                                  )
-                                })}
-
-
-                              </tbody>
-                            </table>
-
-                            <table className="line-items-container has-bottom-border">
-                              <thead>
-                                <tr>
-                                  {/* <th>Payment Info</th> */}
-                                  <th>Due By</th>
-                                  <th>Total Due</th>
-                                </tr>
-                              </thead>
-                              <tbody>
-                                <tr>
-                                  <td className="large">{orderupdate_date}</td>
-                                  <td className="large total">{totalinvoice}</td>
-                                </tr>
-                              </tbody>
-
-                            </table>
-                            <div className="footer">
-                              <div className="footer-info">
-                                <span>hello@useanvil.com</span> |
-                                <span>555 444 6666</span> |
-                                <span>useanvil.com</span>
-                              </div>
-                              <div className="footer-thanks">
-                                <img src="https://github.com/anvilco/html-pdf-invoice-template/raw/main/img/heart.png" alt="heart" />
-                                <span>Thank you!</span>
-                              </div>
+                          <div id="invoice-POS">
+                          <center id="top">
+                            <div className="logo"></div>
+                            <div className="info">
+                              <h2>SBISTechs Inc</h2>
                             </div>
+                          </center>
+
+                          <div id="mid">
+                            <div className="info">
+                              <h2>Contact Info</h2>
+                              <p>
+                                Address : street city, state 0000
+                                Email   : JohnDoe@gmail.com
+                                Phone   : 555-555-5555
+                              </p>
+                            </div>
+                          </div>
+
+                          <div id="bot">
+
+                            <div id="table">
+                              <table>
+                                <tr className="tabletitle">
+                                  <td className="item"><h2>المنتج</h2></td>
+                                  <td className="Hours"><h2>الكمية</h2></td>
+                                  <td className="Hours"><h2>السعر</h2></td>
+                                  <td className="Rate"><h2>التكلفه</h2></td>
+                                </tr>
+                                {list_products_order.map((item, i) => {
+                                  console.log(`list_products_order ${list_products_order}`)
+                                  return (
+                                    <tr className="service">
+                                      <td className="tableitem"><p className="itemtext">{item.name}</p></td>
+                                      <td className="tableitem"><p className="itemtext">{item.quantity}</p></td>
+                                      <td className="tableitem"><p className="itemtext">{item.price}</p></td>
+                                      <td className="tableitem"><p className="itemtext">{item.totalprice}</p></td>
+                                    </tr>)
+                                }
+                                )}
+
+
+                                <tr className="tabletitle">
+                                  <td className="Rate" colspan="3"><h2>المجموع</h2></td>
+                                  <td className="payment"><h2>{totalinvoice}</h2></td>
+                                </tr>
+                                <tr className="tabletitle">
+                                  <td className="Rate" colspan="3"><h2>ضرائب</h2></td>
+                                  <td className="payment"><h2>{totalinvoice * 0.14}</h2></td>
+                                </tr>
+                                <tr className="tabletitle">
+                                  <td className="Rate" colspan="3"><h2>الاجمالي</h2></td>
+                                  <td className="payment"><h2>{totalinvoice + totalinvoice * 0.14}</h2></td>
+                                </tr>
+                              </table>
+                            </div>
+
+                            <div id="legalcopy">
+                              <p className="legal"><strong>Thank you for your business!</strong>  Payment is expected within 31 days; please process this invoice within that time. There will be a 5% interest charge per month on late invoices.
+                              </p>
+                            </div>
+
+                          </div>
+                        </div>
                           </div>
                           <div className="total-order">
                             <button className='total-order-btn' onClick={() => checkout()}>طلب الحساب</button>

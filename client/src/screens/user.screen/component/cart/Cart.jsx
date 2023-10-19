9ac4933ade8f65b59a870258f2c6f56b878eb0cb
beshoynady <bishoin2017@gmail.com>
2023-10-19 16:13:52 +0300

    totalinvoice

--- client/src/screens/user.screen/component/cart/Cart.jsx
+++ client/src/screens/user.screen/component/cart/Cart.jsx
@@ -168,79 +168,6 @@ const Cart = (props) => {
 
                           </div>
                         </div>
-                        {/* <table className="invoice-info-container">
-                          <tbody className='tbody-info'>
-                            <tr>
-                              {id ?<td rowSpan="2" className="client-name">طاولة: {usertitle(id)}</td>
-                              :userlogininfo?<td rowSpan="2" className="client-name">عميل: {usertitle(userlogininfo.id)}</td> 
-                              :""}
-                              
-                              <td rowSpan="2">
-                                كافيار
-                              </td>
-                            </tr>
-                            <tr>
-                              <td>
-                                Invoice Date: <strong>{orderupdate_date}</strong>
-                              </td>
-                              <td>
-                                Invoice No: <strong>{myorder.serial}</strong>
-                              </td>
-                            </tr>
-                          </tbody>
-                        </table>
-                        <table className="line-items-container">
-                          <thead>
-                            <tr>
-                              <th className="bold heading-name">المنتج</th>
-                              <th className="bold heading-quantity">الكمية</th>
-                              <th className="bold heading-price">السعر</th>
-                              <th className="bold heading-subtotal">التكلفه</th>
-                            </tr>
-                          </thead>
-                          <tbody>
-                            {list_products_order.map((item, i) => {
-                              console.log(`list_products_order ${list_products_order}`)
-                              return (
-                                <tr key={i}>
-                                  <td className="bold heading-name">{item.name}</td>
-                                  <td className="bold heading-quantity">{item.quantity}</td>
-                                  <td className="bold heading-price">{item.price}</td>
-                                  <td className="bold heading-subtotal">{item.totalprice}</td>
-                                </tr>
-                              )
-                            })}
-
-
-                          </tbody>
-                        </table>
-
-                        <table className="line-items-container has-bottom-border">
-                          <thead>
-                            <tr>
-                              <th>Due By</th>
-                              <th>Total Due</th>
-                            </tr>
-                          </thead>
-                          <tbody>
-                            <tr>
-                              <td className="large">{orderupdate_date}</td>
-                              <td className="large total">{totalinvoice}</td>
-                            </tr>
-                          </tbody>
-
-                        </table>
-                        <div className="footer">
-                          <div className="footer-info">
-                            <span>hello@useanvil.com</span> |
-                            <span>555 444 6666</span> |
-                            <span>useanvil.com</span>
-                          </div>
-                          <div className="footer-thanks">
-                            <img src="https://github.com/anvilco/html-pdf-invoice-template/raw/main/img/heart.png" alt="heart" />
-                            <span>Thank you!</span>
-                          </div>
-                        </div> */}
                       </div>
                       <div className="total-order">
                         {id ? <button className='total-order-btn' onClick={() => checkout()}>طلب الحساب</button> : ""}
@@ -248,7 +175,7 @@ const Cart = (props) => {
                         <button className='total-order-btn' onClick={handlePrint}>طباعه</button>
                         <div className='total-order-details'>
                           <h2>الاجمالي</h2>
-                          <p>{totalinvoice}</p>
+                          <p>{totalinvoice + (totalinvoice*.14)}</p>
                         </div>
 
                       </div>

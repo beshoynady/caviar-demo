113201d3efc1310b06526c150a4b70c05210a484
beshoynady <bishoin2017@gmail.com>
2023-10-26 20:38:56 +0300

    Number

--- client/src/screens/management/manag.component/products/Products.jsx
+++ client/src/screens/management/manag.component/products/Products.jsx
@@ -416,7 +416,7 @@ const Products = () => {
                       <div className="modal-body">
                         <div className="form-group">
                           <label>الاسم</label>
-                          <select  form="carform"  onChange={(e) => {console.log(AllStockItems.find(s=>s._id ==e.target.value).costOfPart); setitemId(e.target.value); setname(AllStockItems.find(s=>s._id ==e.target.value).itemName); setunit(AllStockItems.find(s=>s._id ==e.target.value).smallUnit); setcostofitem(AllStockItems.find(s=>s._id ==e.target.value).costOfPart)}}>
+                          <select  form="carform"  onChange={(e) => {console.log(AllStockItems.find(s=>s._id ==e.target.value).costOfPart); setitemId(e.target.value); setname(AllStockItems.find(s=>s._id ==e.target.value).itemName); setunit(AllStockItems.find(s=>s._id ==e.target.value).smallUnit); setcostofitem(Number(AllStockItems.find(s=>s._id ==e.target.value).costOfPart))}}>
                             <option >اختر</option>
                             {AllStockItems && AllStockItems.map((item, i) => {
                               return(
@@ -428,7 +428,7 @@ const Products = () => {
                         </div>
                         <div className="form-group">
                           <label>التكلفة</label>
-                          <input type='text' className="form-control" defaultValue={costofitem} readOnly required />
+                          <input type='Number' className="form-control" defaultValue={costofitem} readOnly required />
                         </div>
                         <div className="form-group">
                           <label>الكمية</label>
@@ -437,7 +437,7 @@ const Products = () => {
                         </div>
                         <div className="form-group">
                           <label>التكلفة الاجمالية</label>
-                          <input type='text' className="form-control" defaultValue={totalcostofitem}  required readOnly />
+                          <input type='Number' className="form-control" defaultValue={totalcostofitem}  required readOnly />
                         </div>
                         <div className="form-group">
                           <button>اضافه جديدة</button>

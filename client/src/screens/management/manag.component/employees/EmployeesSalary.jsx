a32c29f6be2ad90a9cde5a74f36735f170797c96
beshoynady <bishoin2017@gmail.com>
2023-11-04 13:52:24 +0200

    setEmployeeName

--- client/src/screens/management/manag.component/employees/EmployeesSalary.jsx
+++ client/src/screens/management/manag.component/employees/EmployeesSalary.jsx
@@ -251,11 +251,11 @@ const getSalaryMovement = async()=>{
                       <div className="modal-body">
                         <div className="form-group">
                           <label>الاسم</label>
-                          <select form="carform" required  onChange={(e) =>{console.log(e.target.name) ;console.log(e.target.innerText) ;setEmployeeName(e.target.name);setEmployeeId(e.target.value)}}>
+                          <select form="carform" required  onChange={(e) =>{console.log(listofemployee.find(e=>e._id == e.target.value).fullname) ;setEmployeeName(listofemployee.find(e=>e._id == e.target.value).fullname);setEmployeeId(e.target.value)}}>
                             <option>اختر</option>
                             {listofemployee.map(employee =>{
                               return(
-                                <option name={employee.fullname} value={employee._id}>{employee.fullname}</option>
+                                <option value={employee._id}>{employee.fullname}</option>
                               )
                             })}
                           </select>

1537fcc6ff8e393b33fcb8f9797a2984388b8ee8
beshoynady <bishoin2017@gmail.com>
2023-11-06 17:11:56 +0200

    addPayRoll

--- client/src/screens/management/manag.component/employees/PayRole.jsx
+++ client/src/screens/management/manag.component/employees/PayRole.jsx
@@ -45,7 +45,7 @@ const PayRole = () => {
     console.log(listofsalarymovement?listofsalarymovement.find(m => m.EmployeeId == "654545f6c10dbaed54e3f0ee"):'')
     listofemployee.map((employee,i) =>{
       console.log(employee._id)
-      const employeemov =listofsalarymovement? listofsalarymovement.filter(m => m.EmployeeId == employee._id):'' 
+      const employeemov =listofsalarymovement? listofsalarymovement.filter(m => m.EmployeeId == "654545f6c10dbaed54e3f0ee"):'' 
       console.log(employeemov)
       if(employeemov.length>0){
         const employeeBonus = employeemov.find(mov=>mov.movement == "مكافأة")

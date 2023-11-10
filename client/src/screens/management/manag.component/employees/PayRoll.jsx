680da27a8bdc4c694970a302d4bd2eaf7515b2c2
beshoynady <bishoin2017@gmail.com>
2023-11-10 11:06:40 +0200

    getSalaryMovement

--- client/src/screens/management/manag.component/employees/PayRoll.jsx
+++ client/src/screens/management/manag.component/employees/PayRoll.jsx
@@ -44,10 +44,10 @@ const PayRoll = () => {
     movement.map((movement) => {
       console.log(new Date(movement.createdAt).getMonth())
     })
-    const filterByMonth = movement.filter((m)=>{Date(m.createdAt).getMonth() == date})
-    // const filterByMonth = movement.filter((m)=>{console.log(Date(m.createdAt)})
-    console.log(filterByMonth)
-    setlistofsalarymovement(filterByMonth)
+    // const filterByMonth = movement.filter((m)=>{Date(m.createdAt).getMonth() == date})
+    // // const filterByMonth = movement.filter((m)=>{console.log(Date(m.createdAt)})
+    // console.log(filterByMonth)
+    // setlistofsalarymovement(filterByMonth)
   }
 
 

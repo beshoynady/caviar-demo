1e399db6f6e8a5059a4944140fc97ee3792cecd5
beshoynady <bishoin2017@gmail.com>
2023-11-13 15:28:24 +0200

    onSubmit

--- client/src/screens/management/manag.component/expenses/Expense.jsx
+++ client/src/screens/management/manag.component/expenses/Expense.jsx
@@ -215,7 +215,7 @@ const ExpenseItem = () => {
                         <div id="editExpensesModal" className="modal fade">
                             <div className="modal-dialog">
                                 <div className="modal-content">
-                                    <form onSubmit={(e) => editExpense(e,expenseId)}>
+                                    <form onSubmit={editExpense}>
                                         <div className="modal-header">
                                             <h4 className="modal-title">تعديل صنف بالمخزن</h4>
                                             <button type="button" className="close" data-dismiss="modal" aria-hidden="true">&times;</button>
@@ -238,7 +238,7 @@ const ExpenseItem = () => {
                         <div id="deleteExpensesModal" className="modal fade">
                             <div className="modal-dialog">
                                 <div className="modal-content">
-                                    <form onSubmit={(e)=>deleteExpense(e,expenseId)}>
+                                    <form onSubmit={deleteExpense}>
                                         <div className="modal-header">
                                             <h4 className="modal-title">حذف منتج</h4>
                                             <button type="button" className="close" data-dismiss="modal" aria-hidden="true">&times;</button>

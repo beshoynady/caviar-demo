a2ff8672486480987f351fc1a146c514c15a8c6e
beshoynady <bishoin2017@gmail.com>
2023-11-14 00:29:15 +0200

    chat reset

--- server/router/Product.router.js
+++ server/router/Product.router.js
@@ -14,7 +14,6 @@ const {
 } = require("../controllers/product.controller");
 
 const multer = require("multer");
-
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "images/");

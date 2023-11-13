b5bd2371f63a5c9d9e7f0b273788b3c25cd8e9df
beshoynady <bishoin2017@gmail.com>
2023-11-13 11:47:21 +0200

    order contr

--- server/controllers/Order.controller.js
+++ server/controllers/Order.controller.js
@@ -1,136 +1,138 @@
-const OrderModel = require('../models/Order.model');
+const OrderModel = require('../models/Order.model')
 
-const createOrder = async (req, res) => {
-  try {
-    const {
-      serial,
-      ordernum,
-      products,
-      table,
-      user,
-      total,
-      totalAfterTax,
-      order_type,
-      notes,
-      help,
-      employee,
-      name,
-      phone,
-      address,
-    } = req.body;
 
-    const newOrder = await OrderModel.create({
-      serial,
-      ordernum,
-      products,
-      table,
-      user,
-      total,
-      totalAfterTax,
-      order_type,
-      notes,
-      help,
-      employee,
-      name,
-      phone,
-      address,
-    });
+const createorder = async (req, res) => {
+    const serial = await req.body.serial;
+    const ordernum = await req.body.ordernum;
+    const products = await req.body.products;
+    const table = await req.body.table;
+    const user = await req.body.user;
+    const total = await req.body.total;
+    const totalAfterTax = await req.body.totalAfterTax;
+    const order_type = await req.body.order_type;
+    const notes = await req.body.notes;
+    const help = await req.body.help;
+    const employee = await req.body.employee
+    const name = await req.body.name
+    const phone = await req.body.phone
+    const address = await req.body.address
 
-    res.status(201).json(newOrder);
-  } catch (err) {
-    res.status(400).json({ message: err.message });
-  }
-};
-
-const getOrder = async (req, res) => {
-  const orderId = req.params.id;
-  try {
-    const order = await OrderModel.findById(orderId);
-    if (order) {
-      res.status(200).json(order);
-    } else {
-      res.status(404).json({ message: 'Order not found' });
+    try {
+        const neworder = await OrderModel.create({
+            serial,
+            ordernum,
+            products,
+            table,
+            user,
+            total,
+            totalAfterTax,
+            order_type,
+            notes,
+            help,
+            employee,
+            name,
+            phone,
+            address
+        });
+        neworder.save();
+        res.status(200).json(neworder)
+    } catch (err) {
+        res.json(err).status(400)
     }
-  } catch (err) {
-    res.status(400).json({ message: err.message });
-  }
-};
+}
 
-const getOrders = async (req, res) => {
-  try {
-    const orders = await OrderModel.find();
-    res.status(200).json(orders);
-  } catch (err) {
-    res.status(400).json({ message: err.message });
-  }
-};
 
-const updateOrder = async (req, res) => {
-  const orderId = req.params.id;
-  try {
-    const existingOrder = await OrderModel.findById(orderId);
-
-    if (!existingOrder) {
-      return res.status(404).json({ message: 'Order not found' });
+const getorder = async (req, res) => {
+    const orderid = await req.params.id
+    try {
+        const order = await OrderModel.findById(orderid)
+        res.status(200).json(order)
+    } catch (err) {
+        res.status(400).json(err)
     }
+}
 
-    const {
-      products,
-      tableid,
-      userid,
-      total,
-      totalAfterTax,
-      status,
-      payment_status,
-      isActive,
-      isAdd,
-      order_type,
-      notes,
-      waiter,
-      help,
-      employee,
-    } = req.body;
 
-    existingOrder.products = products;
-    existingOrder.table = tableid;
-    existingOrder.user = userid;
-    existingOrder.total = total;
-    existingOrder.totalAfterTax = totalAfterTax;
-    existingOrder.status = status;
-    existingOrder.payment_status = payment_status;
-    existingOrder.help = help;
-    existingOrder.isActive = isActive;
-    existingOrder.isAdd = isAdd;
-    existingOrder.employee = employee;
-    existingOrder.order_type = order_type;
-    existingOrder.notes = notes;
-    existingOrder.waiter = waiter;
 
-    const updatedOrder = await existingOrder.save();
-    res.status(200).json(updatedOrder);
-  } catch (err) {
-    res.status(400).json({ message: err.message });
-  }
-};
-
-const deleteOrder = async (req, res) => {
-  const orderId = req.params.id;
-  try {
-    const deletedOrder = await OrderModel.findByIdAndDelete(orderId);
-    if (deletedOrder) {
-      res.status(200).json(deletedOrder);
-    } else {
-      res.status(404).json({ message: 'Order not found' });
+const getorders = async (req, res) => {
+    try {
+        const orders = await OrderModel.find()
+        res.status(200).json(orders)
+    } catch (err) {
+        res.status(400).json(err)
     }
-  } catch (err) {
-    res.status(400).json({ message: err.message });
-  }
-};
+}
+const updateorder = async (req, res) => {
+    const orderid = await req.params.id;
+    const products = await req.body.products;
+    const table = await req.body.tableid;
+    const user = await req.body.userid;
+    const total = await req.body.total;
+    const totalAfterTax = await req.body.totalAfterTax;
+    const status = await req.body.status;
+    const payment_status = await req.body.payment_status;
+    const isActive = await req.body.isActive;
+    const isAdd = await req.body.isAdd;
+    const order_type = await req.body.order_type
+    const notes = await req.body.notes
+    const waiter = await req.body.waiter
+    const help = await req.body.help
+    const employee = await req.body.employee
+    try {
+        const updatedorder = await OrderModel.findByIdAndUpdate(orderid, {
+            products,
+            table,
+            user,
+            total,
+            totalAfterTax,
+            status,
+            payment_status,
+            help,
+            isActive,
+            isAdd,
+            employee,
+            order_type,
+            notes,
+            waiter
+        })
+        await updatedorder.save();
+        res.status(200).json(updatedorder)
+    } catch (err) {
+        res.status(400).json(err)
+    }
+}
+
+// const addproduct=async()=>{
+//     const orderid = req.params.id;
+//     const products = req.body.products;
+//     try{
+//         const addorder = await OrderModel.findByIdAndUpdate(orderid,{$push:{
+//             products:products}
+//             })
+//             addorder.save();
+//             res.status(200).json(addorder)
+//     }catch(err){
+//         res.status(400).json(err)
+//         }
+// }
+
+const deleteorder = async (req, res) => {
+    const orderid = req.params.id;
+    try {
+        const deletedorder = await OrderModel.findByIdAndDelete(orderid)
+        res.status(200).json(deletedorder)
+    } catch (err) {
+        res.status(400).json(err)
+    }
+}
 
 module.exports = {
-  createOrder,
-  getOrder,
-  getOrders,
-  updateOrder,
-  deleteOrder,
-};
+    createorder,
+    getorder,
+    getorders,
+    updateorder,
+    deleteorder
+}
+
+
+

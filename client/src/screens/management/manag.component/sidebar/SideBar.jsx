2b8189e67f21b55b62254be67555474607741d2b
beshoynady <bishoin2017@gmail.com>
2023-11-14 21:54:12 +0200

    sidebar

--- client/src/screens/management/manag.component/sidebar/SideBar.jsx
+++ client/src/screens/management/manag.component/sidebar/SideBar.jsx
@@ -27,156 +27,154 @@ const SideBar = () => {
         ({ userlogininfo, employeelogout }) => {
           return (
             <>
-            <div ref={sidebarRef} className="sidebar close">
-              <div className="logo-details">
-                <i className='bx bxl-c-plus-plus'></i>
-                <span className="logo_name">CAVIAR</span>
-              </div>
-              <ul className="nav-links">
-                {/* Dashboard */}
-                <li>
-                  <Link to="/management">
-                    <span className="material-symbols-outlined icon">dashboard</span>
-                    <span className="link_name">Dashboard</span>
-                  </Link>
-                  <ul className="sub-menu blank">
-                    <li><Link to="/" className="link_name">Dashboard</Link></li>
-                  </ul>
-                </li>
-  
-                {/* POS */}
-                <li>
-                  <Link to="pos">
-                    <span className="material-symbols-outlined icon">point_of_sale</span>
-                    <span className="link_name">POS</span>
-                  </Link>
-                  <ul className="sub-menu blank">
-                    <li><Link to="pos" className="link_name">POS</Link></li>
-                  </ul>
-                </li>
-  
-                {/* Waiter */}
-                <li>
-                  <Link to="waiter">
-                    <span className="material-symbols-outlined icon">concierge</span>
-                    <span className="link_name">الويتر</span>
-                  </Link>
-                  <ul className="sub-menu blank">
-                    <li><Link to="waiter" className="link_name">الويتر</Link></li>
-                  </ul>
-                </li>
-  
-                {/* Kitchen */}
-                <li>
-                  <Link to="kitchen">
-                    <span className="material-symbols-outlined icon">cooking</span>
-                    <span className="link_name">المطبخ</span>
-                  </Link>
-                  <ul className="sub-menu blank">
-                    <li><Link to="kitchen" className="link_name">المطبخ</Link></li>
-                  </ul>
-                </li>
-  
-                {/* Orders */}
-                <li>
-                  <Link to="orders">
-                    <span className="material-symbols-outlined icon">list_alt</span>
-                    <span className="link_name">الاوردرات</span>
-                  </Link>
-                  <ul className="sub-menu blank">
-                    <li><Link to="orders" className="link_name">الاوردرات</Link></li>
-                  </ul>
-                </li>
-  
-                {/* Tables */}
-                <li>
-                  <Link to="tables">
-                    <span className="material-symbols-outlined icon">table_restaurant</span>
-                    <span className="link_name">الطاولة</span>
-                  </Link>
-                  <ul className="sub-menu blank">
-                    <li><Link to="tables" className="link_name">الطاولة</Link></li>
-                  </ul>
-                </li>
-  
-                {/* Menu */}
-                <li ref={arrowRefs.arrowmen} onClick={() => openSubMenu(arrowRefs.arrowmen)}>
-                  <div className="iocn-link">
-                    <a href="#">
-                      <span className="material-symbols-outlined icon">restaurant_menu</span>
-                      <span className="link_name">المنيو</span>
-                    </a>
-                    <i className='bx bxs-chevron-down arrow'></i>
-                  </div>
-                  <ul className="sub-menu">
-                    <li><a className="link_name" href="#">المنيو</a></li>
-                    <li><Link to="category">التصنيفات</Link></li>
-                    <li><Link to="products">الاطباق</Link></li>
-                    <li><Link to="productrecipe">التكاليف</Link></li>
-                  </ul>
-                </li>
-  
-                {/* Employees */}
-                <li ref={arrowRefs.arrowemp} onClick={() => openSubMenu(arrowRefs.arrowemp)}>
-                  <div className="iocn-link">
-                    <a href="#">
-                      <span className="material-symbols-outlined icon">group_add</span>
-                      <span className="link_name">الموظفون</span>
-                    </a>
-                    <i className='bx bxs-chevron-down arrow'></i>
-                  </div>
-                  <ul className="sub-menu">
-                    <li><a className="link_name" href="#">الموظفون</a></li>
-                    <li><Link to="employees">البيانات</Link></li>
-                    <li><Link to="employeessalary">تعاملات</Link></li>
-                    <li><Link to="payroll">المرتبات</Link></li>
-                  </ul>
-                </li>
-  
-                {/* Stock */}
-                <li ref={arrowRefs.arrowsto} onClick={() => openSubMenu(arrowRefs.arrowsto)}>
-                  <div className="iocn-link">
-                    <a href="#">
-                      <span className="material-symbols-outlined icon">receipt_long</span>
-                      <span className="link_name">المخزن</span>
-                    </a>
-                    <i className='bx bxs-chevron-down arrow'></i>
-                  </div>
-                  <ul className="sub-menu">
-                    <li><a className="link_name" href="#">المخزن</a></li>
-                    <li><Link to="categoryStock">تصنيفات</Link></li>
-                    <li><Link to="stockitem">الاصناف</Link></li>
-                    <li><Link to="stockmang">حركه المخزن</Link></li>
-                  </ul>
-                </li>
-  
-                {/* Expenses */}
-                <li ref={arrowRefs.arrowsexp} onClick={() => openSubMenu(arrowRefs.arrowsexp)}>
-                  <div className="iocn-link">
-                    <a href="#">
-                      <span className="material-symbols-outlined arrow icon">request_page</span>
-                      <span className="link_name">المصروفات</span>
-                    </a>
-                    <i className='bx bxs-chevron-down arrow'></i>
-                  </div>
-                  <ul className="sub-menu">
-                    <li><a className="link_name" href="#">المصروفات</a></li>
-                    <li><Link to="expense">المصروفات</Link></li>
-                    <li><Link to="dailyexpense">تسجيل مصروف</Link></li>
-                  </ul>
-                </li>
+              <div ref={sidebarRef} className="sidebar close">
+                <div className="logo-details">
+                  <i className='bx bxl-c-plus-plus'></i>
+                  <span className="logo_name">CAVIAR</span>
+                </div>
+                <ul className="nav-links">
+                  {/* Dashboard */}
                   <li>
-                    <div className="profile-details">
-                      <div className="profile-content">
-                        <i className='bx bx-log-out' onClick={employeelogout}></i>
-                      </div>
-                      <div className="name-job">
-                        <div className="profile_name">{userlogininfo ? userlogininfo.employeeinfo.username : ''}</div>
-                        <div className="job">{userlogininfo ? userlogininfo.employeeinfo.role : ''}</div>
-                      </div>
+                    <Link to="/management">
+                      <span className="material-symbols-outlined icon">dashboard</span>
+                      <span className="link_name">Dashboard</span>
+                    </Link>
+                    <ul className="sub-menu blank">
+                      <li><Link to="/" className="link_name">Dashboard</Link></li>
+                    </ul>
+                  </li>
+
+                  {/* POS */}
+                  <li>
+                    <Link to="pos">
+                      <span className="material-symbols-outlined icon">point_of_sale</span>
+                      <span className="link_name">POS</span>
+                    </Link>
+                    <ul className="sub-menu blank">
+                      <li><Link to="pos" className="link_name">POS</Link></li>
+                    </ul>
+                  </li>
+
+                  {/* Waiter */}
+                  <li>
+                    <Link to="waiter">
+                      <span className="material-symbols-outlined icon">concierge</span>
+                      <span className="link_name">الويتر</span>
+                    </Link>
+                    <ul className="sub-menu blank">
+                      <li><Link to="waiter" className="link_name">الويتر</Link></li>
+                    </ul>
+                  </li>
+
+                  {/* Kitchen */}
+                  <li>
+                    <Link to="kitchen">
+                      <span className="material-symbols-outlined icon">cooking</span>
+                      <span className="link_name">المطبخ</span>
+                    </Link>
+                    <ul className="sub-menu blank">
+                      <li><Link to="kitchen" className="link_name">المطبخ</Link></li>
+                    </ul>
+                  </li>
+
+                  {/* Orders */}
+                  <li>
+                    <Link to="orders">
+                      <span className="material-symbols-outlined icon">list_alt</span>
+                      <span className="link_name">الاوردرات</span>
+                    </Link>
+                    <ul className="sub-menu blank">
+                      <li><Link to="orders" className="link_name">الاوردرات</Link></li>
+                    </ul>
+                  </li>
+
+                  {/* Tables */}
+                  <li>
+                    <Link to="tables">
+                      <span className="material-symbols-outlined icon">table_restaurant</span>
+                      <span className="link_name">الطاولة</span>
+                    </Link>
+                    <ul className="sub-menu blank">
+                      <li><Link to="tables" className="link_name">الطاولة</Link></li>
+                    </ul>
+                  </li>
+
+                  {/* Menu */}
+                  <li ref={arrowRefs.arrowmen} onClick={() => openSubMenu(arrowRefs.arrowmen)}>
+                    <div className="iocn-link">
+                      <a href="#">
+                        <span className="material-symbols-outlined icon">restaurant_menu</span>
+                        <span className="link_name">المنيو</span>
+                      </a>
+                      <i className='bx bxs-chevron-down arrow'></i>
                     </div>
+                    <ul className="sub-menu">
+                      <li><a className="link_name" href="#">المنيو</a></li>
+                      <li><Link to="category">التصنيفات</Link></li>
+                      <li><Link to="products">الاطباق</Link></li>
+                      <li><Link to="productrecipe">التكاليف</Link></li>
+                    </ul>
+                  </li>
+
+                  {/* Employees */}
+                  <li ref={arrowRefs.arrowemp} onClick={() => openSubMenu(arrowRefs.arrowemp)}>
+                    <div className="iocn-link">
+                      <a href="#">
+                        <span className="material-symbols-outlined icon">group_add</span>
+                        <span className="link_name">الموظفون</span>
+                      </a>
+                      <i className='bx bxs-chevron-down arrow'></i>
+                    </div>
+                    <ul className="sub-menu">
+                      <li><a className="link_name" href="#">الموظفون</a></li>
+                      <li><Link to="employees">البيانات</Link></li>
+                      <li><Link to="employeessalary">تعاملات</Link></li>
+                      <li><Link to="payroll">المرتبات</Link></li>
+                    </ul>
+                  </li>
+
+                  {/* Stock */}
+                  <li ref={arrowRefs.arrowsto} onClick={() => openSubMenu(arrowRefs.arrowsto)}>
+                    <div className="iocn-link">
+                      <a href="#">
+                        <span className="material-symbols-outlined icon">receipt_long</span>
+                        <span className="link_name">المخزن</span>
+                      </a>
+                      <i className='bx bxs-chevron-down arrow'></i>
+                    </div>
+                    <ul className="sub-menu">
+                      <li><a className="link_name" href="#">المخزن</a></li>
+                      <li><Link to="categoryStock">تصنيفات</Link></li>
+                      <li><Link to="stockitem">الاصناف</Link></li>
+                      <li><Link to="stockmang">حركه المخزن</Link></li>
+                    </ul>
+                  </li>
+
+                  {/* Expenses */}
+                  <li ref={arrowRefs.arrowsexp} onClick={() => openSubMenu(arrowRefs.arrowsexp)}>
+                    <div className="iocn-link">
+                      <a href="#">
+                        <span className="material-symbols-outlined arrow icon">request_page</span>
+                        <span className="link_name">المصروفات</span>
+                      </a>
+                      <i className='bx bxs-chevron-down arrow'></i>
+                    </div>
+                    <ul className="sub-menu">
+                      <li><a className="link_name" href="#">المصروفات</a></li>
+                      <li><Link to="expense">المصروفات</Link></li>
+                      <li><Link to="dailyexpense">تسجيل مصروف</Link></li>
+                    </ul>
                   </li>
                 </ul>
+                <div className="profile-details">
+                  <div className="profile-content">
+                    <i className='bx bx-log-out' onClick={employeelogout}></i>
+                  </div>
+                  <div className="name-job">
+                    <div className="profile_name">{userlogininfo ? userlogininfo.employeeinfo.username : ''}</div>
+                    <div className="job">{userlogininfo ? userlogininfo.employeeinfo.role : ''}</div>
+                  </div>
+                </div>
               </div>
               <section className="home-section" onClick={openSidebar}>
                 <div className="home-content">

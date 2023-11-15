b26b62dafe7baa6b76a95725cf570d67085fa228
beshoynady <bishoin2017@gmail.com>
2023-11-15 19:30:39 +0200

    error

--- client/src/App.js
+++ client/src/App.js
@@ -703,7 +703,7 @@ function App() {
         toast('هذا المستخدم غير مصرح له بالدخول')
       }
     } catch (error) {
-      console.log(error.data)
+      console.log(error)
       toast(error)
 
     }

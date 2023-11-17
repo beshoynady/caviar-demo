59b722c99af26c97ce28d3a2c7612a1d99a75716
beshoynady <bishoin2017@gmail.com>
2023-11-17 16:45:35 +0200

    EmployeesSalary

--- client/src/screens/management/manag.component/employees/EmployeesSalary.jsx
+++ client/src/screens/management/manag.component/employees/EmployeesSalary.jsx
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import { detacontext } from '../../../../App';
-
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+const Joi = require('joi');
 
 const EmployeesSalary = () => {
 

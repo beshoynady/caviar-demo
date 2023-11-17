8189a8d151891c32eea3287ea42e9ff0db4d1bab
beshoynady <bishoin2017@gmail.com>
2023-11-17 13:05:01 +0200

    Login

--- client/src/screens/management/manag.component/login/Login.jsx
+++ client/src/screens/management/manag.component/login/Login.jsx
@@ -1,60 +1,48 @@
-import React, { useState } from 'react';
-import './Login.css';
-import { detacontext } from '../../../../App';
+import React,{useState} from 'react'
+import './Login.css'
+import { detacontext } from '../../../../App'
 import { ToastContainer, toast } from 'react-toastify';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Login = () => {
-  const [phone, setphone] = useState('');
-  const [password, setpassword] = useState('');
-  return (
-    <detacontext.Consumer>
-      {({ employeelogin }) => {
-        return (
-          <div className="login-container">
-            <ToastContainer />
-            <div className="container-fluid">
-              <div className="row justify-content-center">
-                <div className="col-md-6 col-lg-4">
-                  <div className="card">
-                    <div className="card-body">
-                      <form
-                        className="login-dash"
-                        onSubmit={(e) => employeelogin(e, phone, password)}
-                      >
-                        <div className="form-group">
-                          <i className="login__icon fas fa-user"></i>
-                          <input
-                            type="text"
-                            className="form-control"
-                            placeholder="Your Phone"
-                            onChange={(e) => setphone(e.target.value)}
-                          />
+const [phone, setphone] = useState('')
+const [password, setpassword] = useState('')
+    return (
+        <detacontext.Consumer>
+            {
+                ({ employeelogin }) => {
+                    return (
+                        <div className="login-container container-lg">
+                            <ToastContainer/>
+                            <div className="screen">
+                                <div className="screen__content">
+                                    <form className="login-dash" onSubmit={(e)=>employeelogin(e,phone, password)} >
+                                        <div className="login__field">
+                                            <i className="login__icon fas fa-user"></i>
+                                            <input type="text" className="login__input" placeholder="Your Phone" onChange={(e)=>setphone(e.target.value)}/>
+                                        </div>
+                                        <div className="login__field">
+                                            <i className="login__icon fas fa-lock"></i>
+                                            <input type="password" className="login__input" placeholder="Password" onChange={(e)=>setpassword(e.target.value)}/>
+                                        </div>
+                                        <button className="button login__submit">
+                                            <span className="button__text">تسجيل الدخول</span>
+                                            <i className="button__icon fas fa-chevron-left"></i>
+                                        </button>
+                                    </form>
+                                </div>
+                                <div className="screen__background">
+                                    <span className="screen__background__shape screen__background__shape4"></span>
+                                    <span className="screen__background__shape screen__background__shape3"></span>
+                                    <span className="screen__background__shape screen__background__shape2"></span>
+                                    <span className="screen__background__shape screen__background__shape1"></span>
+                                </div>
+                            </div>
                         </div>
-                        <div className="form-group">
-                          <i className="login__icon fas fa-lock"></i>
-                          <input
-                            type="password"
-                            className="form-control"
-                            placeholder="Password"
-                            onChange={(e) => setpassword(e.target.value)}
-                          />
-                        </div>
-                        <button className="btn btn-primary btn-block">
-                          <span className="button__text">تسجيل الدخول</span>
-                          <i className="button__icon fas fa-chevron-left"></i>
-                        </button>
-                      </form>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        );
-      }}
-    </detacontext.Consumer>
-  );
-};
+                    )
+                }
+            }
+        </detacontext.Consumer>
+    )
+}
 
-export default Login;
+export default Login

5ca909790c682b695ccebe4e8cf2f9057ff30ef2
beshoynady <bishoin2017@gmail.com>
2023-11-24 17:15:52 +0200

    MenuCard

--- client/src/screens/user.screen/component/menu/Menu-card/Menu-card.jsx
+++ client/src/screens/user.screen/component/menu/Menu-card/Menu-card.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './Menu-card.css';
+// import './Menu-card.css';
 import { detacontext } from '../../../../../App';
 import { Card, Button } from 'react-bootstrap';
 

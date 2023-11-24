fca46ff0c37193b31437c456d8c191baffea9c76
beshoynady <bishoin2017@gmail.com>
2023-11-24 17:35:53 +0200

    MenuCard

--- client/src/screens/user.screen/component/menu/Menu-card/Menu-card.jsx
+++ client/src/screens/user.screen/component/menu/Menu-card/Menu-card.jsx
@@ -1,67 +1,63 @@
 import React, { useState } from 'react';
-import { Card, Button, Form, FormControl, InputGroup } from 'react-bootstrap';
+import './Menu-card.css';
 import { detacontext } from '../../../../../App';
 
 const MenuCard = () => {
-  const [noteArea, setNoteArea] = useState(false);
-  const [productid, setProductid] = useState('');
+   const [noteArea, setnoteArea] = useState(false)
+   const [productid, setproductid] = useState('')
+   return (
+      <detacontext.Consumer>
+         {
+            ({ allProducts, categoryid, additemtocart, deleteitems, increment, descrement, setproductnote, addnotrstoproduct, itemid }) => {
+               return (
+                  <div className="card-group">
+                     {allProducts.filter(pro => pro.category === categoryid).map((product, index) => {
+                        return (
+                           <div className="menu-card" key={index}>
+                              <img className='img-card' src={`https://raw.githubusercontent.com/beshoynady/restaurant-api/main/server/images/${product.image}`} alt="" />
+                              {product._id == productid & noteArea == true ? <form onSubmit={(e) => { addnotrstoproduct(e, product._id);; setnoteArea(!noteArea) }}>
+                                 <textarea placeholder='اضف تعليماتك الخاصة بهذا الطبق' name="note" cols="100" rows="3" onChange={(e) => { setproductnote(e.target.value) }}></textarea>
+                                 <div className='note-btn'>
+                                    <button>تاكيد</button>
+                                    <button onClick={() => setnoteArea(!noteArea)}>الغاء</button>
+                                 </div>
+                              </form> : ''}
 
-  return (
-    <detacontext.Consumer>
-      {({ allProducts, categoryid, additemtocart, deleteitems, increment, descrement, setproductnote, addnotrstoproduct, itemid }) => {
-                           return (
-                              <div className="d-flex flex-wrap">
-                                {allProducts.filter(pro => pro.category === categoryid).map((product, index) => (
-                                  <Card className="m-3" key={index} style={{ width: '18rem' }}>
-                                    <Card.Img variant="top" src={`https://raw.githubusercontent.com/beshoynady/restaurant-api/main/server/images/${product.image}`} />
-                                    <Card.Body>
-                                      {product._id === productid && noteArea ? (
-                                        <Form onSubmit={(e) => { addnotrstoproduct(e, product._id); setNoteArea(!noteArea) }}>
-                                          <Form.Control as="textarea" placeholder="اضف تعليماتك الخاصة بهذا الطبق" onChange={(e) => setproductnote(e.target.value)} />
-                                          <div className="mt-2 d-flex justify-content-end">
-                                            <Button type="submit" className="mr-2">تأكيد</Button>
-                                            <Button variant="secondary" onClick={() => setNoteArea(!noteArea)}>الغاء</Button>
-                                          </div>
-                                        </Form>
-                                      ) : (
-                                        <div>
-                                          <Card.Title>{product.name}</Card.Title>
-                                          <Card.Text>{product.description}</Card.Text>
-                                          <InputGroup className="mb-3">
-                                            <InputGroup.Prepend>
-                                              <Button variant="outline-secondary" onClick={() => descrement(product._id)}>-</Button>
-                                            </InputGroup.Prepend>
-                                            <FormControl readOnly value={product.quantity} />
-                                            <InputGroup.Append>
-                                              <Button variant="outline-secondary" onClick={() => increment(product._id)}>+</Button>
-                                            </InputGroup.Append>
-                                          </InputGroup>
-                                          <Card.Text>
-                                            {product.discount > 0 ? (
-                                              <span>{product.price - product.discount} <del>{product.price}</del></span>
-                                            ) : (
-                                              <span>{product.price} ج</span>
-                                            )}
-                                          </Card.Text>
-                                          <div>
-                                            {itemid.includes(product._id) && product.quantity > 0 ? (
-                                              <Button variant="danger" onClick={() => deleteitems(product._id)}>احذف من الطلبات</Button>
-                                            ) : (
-                                              <Button onClick={() => { if (product.quantity > 0) { additemtocart(product._id) } }}>اضف الى طلباتي</Button>
-                                            )}
-                                          </div>
-                                        </div>
-                                      )}
-                                    </Card.Body>
-                                  </Card>
-                                ))}
+                              <div className="detalis">
+                                 <div className='product-det'>
+                                    <div className='product-name'>
+                                       <h2>{product.name}</h2>
+                                       <span className="material-symbols-outlined" onClick={() => { setnoteArea(!noteArea); setproductid(product._id) }}>note_alt</span>
+                                    </div>
+                                    <p>{product.description}</p>
+                                 </div>
+                                 <div className="price">
+                                    <div className="counter">
+                                       <button className='symb' onClick={() => descrement(product._id)}>-</button>
+                                       <span className='num'>{product.quantity}</span>
+                                       <button className='symb' onClick={() => increment(product._id)}>+</button>
+                                    </div>
+                                    {product.discount > 0 ?
+                                       <p>{product.price - product.discount}<sup><del>{product.price}</del></sup></p> :
+                                       <p>{product.price} ج</p>}
+                                 </div>
+                                 <div className='card-btn'>
+                                    {itemid.filter((i) => i == product._id).length > 0 && product.quantity > 0 ?
+                                       <button className='delfromcart' onClick={() => { deleteitems(product._id) }}>احذف من الطلبات</button>
+                                       : <button className='addtocart' onClick={() => { if (product.quantity > 0) { additemtocart(product._id) } }}>اضف الي طلباتي</button>}
+                                 </div>
                               </div>
+                           </div>
+                        )
+                     }
+                     )}
+                  </div>
+               )
+            }
+         }
+      </detacontext.Consumer>
 
-                           )}
-
-      }
-    </detacontext.Consumer>
-  );
+   )
 }
 
-export default MenuCard;
+export default MenuCard

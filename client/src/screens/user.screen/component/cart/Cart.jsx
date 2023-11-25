bb8ac697cbbe7354ad70c1fb33d08f66d8c8897a
beshoynady <bishoin2017@gmail.com>
2023-11-25 09:50:29 +0200

    invoice

--- client/src/screens/user.screen/component/cart/Cart.jsx
+++ client/src/screens/user.screen/component/cart/Cart.jsx
@@ -117,83 +117,151 @@ const Cart = (props) => {
                     <div className="invoice side" >
                       <div ref={printContainer} className="side-content">
                       <div className="container">
-                      {/* Invoice Header */}
-                      <div className="invoice-header" style={{ backgroundColor: '#343a40', color: '#ffffff', padding: '20px', textAlign: 'center' }}>
-                        <h2>Restaurant Name</h2>
-                        <p>Invoice #1234 | Date: November 25, 2023 | Time: 14:30</p>
-                        {/* Buttons */}
-                        <div>
-                          <button className="btn btn-primary mr-2" onClick={() => {/* Function for download invoice */ }}>Download Invoice</button>
-                          <button className="btn btn-success" onClick={() => {/* Function for printing invoice */ }}>Print Invoice</button>
+                          {/* Buttons */}
+                          <div>
+                            <button className="btn btn-primary mr-2" onClick={() => {/* Function for download invoice */ }}>Download Invoice</button>
+                            <button className="btn btn-success" onClick={() => {/* Function for printing invoice */ }}>Print Invoice</button>
+                          </div>
+                          {/* Invoice Header */}
+                          <div className="invoice-header" style={{ backgroundColor: '#343a40', color: '#ffffff', padding: '20px', textAlign: 'center' }}>
+                            <h2>Restaurant Name</h2>
+                            <p>Invoice #1234 | Date: November 25, 2023 | Time: 14:30</p>
+                          </div>
+
+                          {/* Customer Information */}
+                          <div className="customer-info" style={{ marginBottom: '20px' }}>
+                            <h4>Customer Details</h4>
+                            <p>Name: John Doe</p>
+                            <p>Mobile: 123-456-7890</p>
+                            <p>Address: 123 Main St, City</p>
+                          </div>
+
+                          {/* Order Details Table */}
+                          <table className="table table-bordered">
+                            <thead className="thead-dark">
+                              <tr>
+                                <th scope="col">Item</th>
+                                <th scope="col">Price</th>
+                                <th scope="col">Quantity</th>
+                                <th scope="col">Total</th>
+                              </tr>
+                            </thead>
+                            <tbody>
+                              {/* Example rows, replace with dynamic data */}
+                              {list_products_order.map((item, i) => (
+                                    <tr key={i}>
+                                      <td>{item.name}</td>
+                                      <td>{item.priceAfterDiscount ? item.priceAfterDiscount : item.price}</td>
+                                      <td>{item.quantity}</td>
+                                      <td>{item.totalprice}</td>
+                                    </tr>
+                                  ))}
+                            </tbody>
+                            <tfoot>
+                              <tr>
+                                <td colSpan="3">Subtotal</td>
+                                <td>{ordersubtotal}</td>
+                              </tr>
+                              {orderdeliveryCost && (
+                                    <tr>
+                                      <td colSpan="3">Delivery</td>
+                                      <td>{orderdeliveryCost}</td>
+                                    </tr>
+                                  )}
+                              <tr>
+                                <td colSpan="3">Tax</td>
+                                <td>{ordertax}</td>
+                              </tr>
+                              <tr>
+                                <td colSpan="3">Total</td>
+                                <td>{ordertotal}</td>
+                              </tr>
+                            </tfoot>
+                          </table>
+
+                          {/* Restaurant Information */}
+                          <div className="restaurant-info" style={{ marginTop: '20px', textAlign: 'center' }}>
+                            <h4>Restaurant Details</h4>
+                            <p>Restaurant Name</p>
+                            <p>Mobile: 987-654-3210</p>
+                            <p>Address: 456 Street, City</p>
+                          </div>
+
+                          {/* Footer */}
+                          <div className="footer" style={{ marginTop: '30px', textAlign: 'center', color: '#828282' }}>
+                            <p>Developed by: <span style={{ color: '#5a6268' }}>esyservice</span></p>
+                          </div>
                         </div>
-                      </div>
+                        {/* <div id="invoice-POS">
+                          <center id="top">
+                            <div className="logo"></div>
+                            <div className="info">
+                              <h2>SBISTechs Inc</h2>
+                            </div>
+                          </center>
 
-                      {/* Customer Information */}
-                      <div className="customer-info" style={{ marginBottom: '20px' }}>
-                        <h4>Customer Details</h4>
-                        <p>Name: John Doe</p>
-                        <p>Mobile: 123-456-7890</p>
-                        <p>Address: 123 Main St, City</p>
-                      </div>
+                          <div id="mid">
+                            <div className="info">
+                              <h2>Contact Info</h2>
+                              <p>
+                                Address : street city, state 0000
+                                Email   : JohnDoe@gmail.com
+                                Phone   : 555-555-5555
+                              </p>
+                            </div>
+                          </div>
 
-                      {/* Order Details Table */}
-                      <table className="table table-bordered">
-                        <thead className="thead-dark">
-                          <tr>
-                            <th scope="col">Item</th>
-                            <th scope="col">Price</th>
-                            <th scope="col">Quantity</th>
-                            <th scope="col">Total</th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                          {/* Example rows, replace with dynamic data */}
-                          <tr>
-                            <td>Item 1</td>
-                            <td>$10</td>
-                            <td>2</td>
-                            <td>$20</td>
-                          </tr>
-                          <tr>
-                            <td>Item 2</td>
-                            <td>$15</td>
-                            <td>1</td>
-                            <td>$15</td>
-                          </tr>
-                        </tbody>
-                        <tfoot>
-                          <tr>
-                            <td colSpan="3">Subtotal</td>
-                            <td>$35</td>
-                          </tr>
-                          <tr>
-                            <td colSpan="3">Delivery</td>
-                            <td>$5</td>
-                          </tr>
-                          <tr>
-                            <td colSpan="3">Tax</td>
-                            <td>$3.50</td>
-                          </tr>
-                          <tr>
-                            <td colSpan="3">Total</td>
-                            <td>$43.50</td>
-                          </tr>
-                        </tfoot>
-                      </table>
+                          <div id="bot">
 
-                      {/* Restaurant Information */}
-                      <div className="restaurant-info" style={{ marginTop: '20px', textAlign: 'center' }}>
-                        <h4>Restaurant Details</h4>
-                        <p>Restaurant Name</p>
-                        <p>Mobile: 987-654-3210</p>
-                        <p>Address: 456 Street, City</p>
-                      </div>
+                            <div id="table">
+                              <table className="table table-striped">
+                                <thead>
+                                  <tr>
+                                    <th scope="col">المنتج</th>
+                                    <th scope="col">الكمية</th>
+                                    <th scope="col">السعر</th>
+                                    <th scope="col">التكلفة</th>
+                                  </tr>
+                                </thead>
+                                <tbody>
+                                  {list_products_order.map((item, i) => (
+                                    <tr key={i}>
+                                      <td>{item.name}</td>
+                                      <td>{item.quantity}</td>
+                                      <td>{item.priceAfterDiscount ? item.priceAfterDiscount : item.price}</td>
+                                      <td>{item.totalprice}</td>
+                                    </tr>
+                                  ))}
+                                  <tr className="tabletitle">
+                                    <td className="Rate" colSpan="3">المجموع</td>
+                                    <td className="payment">{ordersubtotal}</td>
+                                  </tr>
+                                  {orderdeliveryCost && (
+                                    <tr className="tabletitle">
+                                      <td className="Rate" colSpan="3">الديلفري</td>
+                                      <td className="payment">{orderdeliveryCost}</td>
+                                    </tr>
+                                  )}
+                                  <tr className="tabletitle">
+                                    <td className="Rate" colSpan="3">ضرائب</td>
+                                    <td className="payment">{ordertax}</td>
+                                  </tr>
+                                  <tr className="tabletitle">
+                                    <td className="Rate" colSpan="3">الإجمالي</td>
+                                    <td className="payment">{ordertotal}</td>
+                                  </tr>
+                                </tbody>
+                              </table>
+                            </div>
 
-                      {/* Footer */}
-                      <div className="footer" style={{ marginTop: '30px', textAlign: 'center', color: '#828282' }}>
-                        <p>Developed by: <span style={{ color: '#5a6268' }}>esyservice</span></p>
-                      </div>
-                    </div>
+
+                            <div id="legalcopy">
+                              <p className="legal"><strong>Thank you for your business!</strong>  Payment is expected within 31 days; please process this invoice within that time. There will be a 5% interest charge per month on late invoices.
+                              </p>
+                            </div>
+
+                          </div>
+                        </div> */}
                       </div>
                       <div className="total-order">
                         {id ? <button className='total-order-btn' onClick={() => checkout()}>طلب الحساب</button> : ""}

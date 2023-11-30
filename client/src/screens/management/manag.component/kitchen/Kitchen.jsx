01aa0de332903b5007696b1ef8c7fb6602f76302
beshoynady <bishoin2017@gmail.com>
2023-11-30 23:13:25 +0200

    Kitchen

--- client/src/screens/management/manag.component/kitchen/Kitchen.jsx
+++ client/src/screens/management/manag.component/kitchen/Kitchen.jsx
@@ -5,163 +5,156 @@ import axios from 'axios'
 
 
 
+import { useState, useEffect, useRef } from 'react';
+import axios from 'axios';
+import { toast } from 'react-toastify'; // Importing toast from 'react-toastify' for notifications
+import 'react-toastify/dist/ReactToastify.css'; // Importing default CSS for toast notifications
+
 const Kitchen = () => {
-  const start = useRef()
-  const ready = useRef()
+  const start = useRef();
+  const ready = useRef();
 
+  const [orderactive, setOrderActive] = useState([]); // State for active orders
+  const [allOrders, setAllOrders] = useState([]); // State for all orders
+  const [waiters, setWaiters] = useState([]); // State for active waiters
+  const [waittime, setWaitTime] = useState(''); // State for waiting time
 
-  const [orderactive, setorderactive] = useState([])
-  const [allOrders, setallOrders] = useState([])
-  const GetPrductstoKit = async () => {
+  // Fetches orders from the API
+  const getOrdersFromAPI = async () => {
     try {
       const orders = await axios.get('https://caviar-api.vercel.app/api/order');
-      // console.log(orders.data)
-      setallOrders(orders.data)
+      setAllOrders(orders.data);
 
-      const orderisctive = await orders.data.filter((order) => order.isActive == true && order.status == 'Approved' || order.status == 'Preparing')
-      // console.log(orderisctive)
-      setorderactive(orderisctive)
+      const activeOrders = orders.data.filter(
+        (order) => order.isActive && (order.status === 'Approved' || order.status === 'Preparing')
+      );
+      setOrderActive(activeOrders);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 
-  const [waiters, setwaiters] = useState([])
-  const getAllWaiter = async () => {
-    const allemployee = await axios.get('https://caviar-api.vercel.app/api/employee')
-    console.log(allemployee)
-    const allwaiter = await allemployee.data.filter((employee) => employee.role == 'waiter')
-    console.log(allwaiter)
-    const waiterActive = await allwaiter.filter((waiter) => waiter.isActive == true)
-    console.log(waiterActive)
-    const listId = []
-    if (waiterActive) {
-      waiterActive.forEach((waiter) => {
-        listId.push(waiter._id)
-      })
-    }
-    console.log(listId)
-    if (listId.length > 0) {
-      setwaiters(listId)
-    }
-  }
+  // Fetches all active waiters from the API
+  const getAllWaiters = async () => {
+    try {
+      const allEmployees = await axios.get('https://caviar-api.vercel.app/api/employee');
+      const allWaiters = allEmployees.data.filter((employee) => employee.role === 'waiter');
+      const waiterActive = allWaiters.filter((waiter) => waiter.isActive);
 
-  // const [waiter, setwaiter] = useState()
+      const waiterIds = waiterActive.map((waiter) => waiter._id);
+      setWaiters(waiterIds);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // Determines the next available waiter to take an order
   const specifiedWaiter = () => {
-    const ordertakewaiter = allOrders.filter((order) => order.waiter != null)
-    console.log(ordertakewaiter)
-    const lastwaiter = ordertakewaiter.length > 0 ? ordertakewaiter[ordertakewaiter.length - 1].waiter : ''
-    // console.log(lastwaiter)
+    const orderTakeWaiter = allOrders.filter((order) => order.waiter !== null);
+    const lastWaiter = orderTakeWaiter.length > 0 ? orderTakeWaiter[orderTakeWaiter.length - 1].waiter : '';
 
-    const indexoflastwaiter = lastwaiter != '' ? waiters.indexOf(lastwaiter) : 0
+    const indexLastWaiter = lastWaiter ? waiters.indexOf(lastWaiter) : 0;
 
-    // console.log(indexoflastwaiter)
-    // console.log(indexoflastwaiter + 1)
-    // console.log(waiters.length)
-    // console.log(waiters)
-    // setwaiter(waiters[indexofwaiter+1])
-    if (waiters.length == indexoflastwaiter + 1) {
-      const waiter = waiters[0]
-      return waiter
+    if (waiters.length === indexLastWaiter + 1) {
+      return waiters[0];
     } else {
-      const waiter = waiters[indexoflastwaiter + 1]
-      return waiter
+      return waiters[indexLastWaiter + 1];
     }
-  }
-  const orderInprogress = async (id) => {
-    // await specifiedWaiter();
-    const waiter = await specifiedWaiter()
-    const status = 'Preparing'
-    const order = await axios.put('https://caviar-api.vercel.app/api/order/' + id, {
-      status, waiter
-    })
-    console.log(order.data)
-    GetPrductstoKit()
-    getAllWaiter()
-  }
+  };
 
+  // Updates an order status to 'Preparing'
+  const orderInProgress = async (id) => {
+    const waiter = specifiedWaiter();
+    const status = 'Preparing';
 
+    try {
+      await axios.put(`https://caviar-api.vercel.app/api/order/${id}`, { status, waiter });
+      getOrdersFromAPI();
+      getAllWaiters();
+      toast.success('Order is in progress!'); // Notifies success in starting order
+    } catch (error) {
+      console.log(error);
+      toast.error('Failed to start order!'); // Notifies failure in starting order
+    }
+  };
+
+  // Updates an order status to 'Prepared'
   const orderDone = async (id) => {
-    const status = 'Prepared'
-    const done = await axios.put('https://caviar-api.vercel.app/api/order/' + id, {
-      status
-    })
-    GetPrductstoKit()
-  }
+    const status = 'Prepared';
 
-  const [waittime, setwaittime] = useState('')
-  const Waitingtime = (t) => {
-    const t1 = new Date(t).getTime()
-    const t2 = new Date().getTime()
-    const Waiting_time = t2 - t1
-    // console.log(Waiting_time)
-    const m = new Date(Waiting_time).getMinutes()
-    // const s = new Date(Waiting_time).getSeconds()
-    // const Waiting_time_m_s = `${m}:${s}`
-    // console.log(Waiting_time_m_s)
-    // setwaittime(Waiting_time_m_s)
-    setwaittime(m)
-    setTimeout(Waitingtime, 60000)
-    // return Waiting_time_m_s
-    return m
-  }
+    try {
+      await axios.put(`https://caviar-api.vercel.app/api/order/${id}`, { status });
+      getOrdersFromAPI();
+      toast.success('Order is prepared!'); // Notifies success in completing order
+    } catch (error) {
+      console.log(error);
+      toast.error('Failed to complete order!'); // Notifies failure in completing order
+    }
+  };
 
+  // Calculates the waiting time for an order
+  const waitingTime = (t) => {
+    const t1 = new Date(t).getTime();
+    const t2 = new Date().getTime();
+    const waitingTime = t2 - t1;
 
-  // useEffect(() => {
-  //   GetPrductstoKit()
-  // }, [waittime])
+    const m = new Date(waitingTime).getMinutes();
+    setWaitTime(m);
 
+    setTimeout(() => waitingTime(t), 60000);
+    return m;
+  };
+
+  // Fetches orders and active waiters on initial render
   useEffect(() => {
-    GetPrductstoKit()
-    getAllWaiter()
-    // specifiedWaiter()
-    // console.log(waiters)
-  }, [])
+    getOrdersFromAPI();
+    getAllWaiters();
+  }, []);
 
   return (
     <detacontext.Consumer>
       {
         ({ usertitle, updatecountofsales }) => {
           return (
-            <div className="container-fluid h-100 overflow-auto bg-transparent py-5 px-3">
-                {
-                  orderactive && orderactive.map((order, i) => {
-                    if (order.products.filter((pr) => pr.isDone == false).length > 0) {
-                      return (
-
-                        <div className="card text-white bg-success mb-3" style={{ maxWidth: "18rem" }} key={i}>
-                          <div className="card-body text-right p-0">
-                            <p className="card-text">طاولة: {order.table != null ? usertitle(order.table) : (order.user ? usertitle(order.user) : '')}</p>
-                            <p className="card-text">رقم الطلب: {order.serial}</p>
-                            <p className="card-text">نوع الطلب: {order.order_type}</p>
-                            {order.waiter ? <p className="card-text">الويتر: {usertitle(order.waiter)}</p> : ""}
-                            <p className="card-text">وقت الاستلام: {new Date(order.createdAt).getHours()}:{new Date(order.createdAt).getMinutes()}</p>
-                            <p className="card-text">الانتظار: {Waitingtime(order.createdAt)} دقيقه</p>
-                            <ul className='list-group list-group-flush'>
-                              {order.products.filter((pr) => pr.isDone === false).map((product, i) => {
-                                return (
-                                  <li className='list-group-item d-flex justify-content-between align-items-center' key={i} style={product.isAdd ? { backgroundColor: 'red' } : {}}>
-                                    <div>
-                                      <p>{i + 1}- {product.name}</p>
-                                      <span> × {product.quantity}</span>
-                                    </div>
-                                    <div>{product.notes}</div>
-                                  </li>
-                                )
-                              })}
-                            </ul>
-                            <div>
-                              {order.status === 'Preparing' ? <button className="btn btn-warning" onClick={() => { orderDone(order._id); updatecountofsales(order._id) }}>تم التنفيذ</button>
-                                : <button className="btn btn-success" onClick={() => orderInprogress(order._id)}>بدء التنفيذ</button>
-                              }
-                            </div>
+            <div className='container-fluid h-100 overflow-auto bg-transparent py-5 px-3'>
+              <ToastContainer/>
+              {
+                orderactive && orderactive.map((order, i) => {
+                  if (order.products.filter((pr) => pr.isDone == false).length > 0) {
+                    return (
+                      <div className="card text-white bg-success" style={{ width: "265px" }}>
+                        <div className="card-body text-right d-flex justify-content-between p-0 m-1">
+                          <p className="card-text"> {order.table != null ? `طاولة: ${usertitle(order.table)}` : (order.user ? `العميل: ${usertitle(order.user)}` : '')}</p>
+                          <p className="card-text">رقم الطلب: {order.serial}</p>
+                          <p className="card-text">نوع الطلب: {order.order_type}</p>
+                          {order.waiter ? <p className="card-text">الويتر: {usertitle(order.waiter)}</p> : ""}
+                          <p className="card-text">وقت الاستلام: {new Date(order.createdAt).getHours()}:{new Date(order.createdAt).getMinutes()}</p>
+                          <p className="card-text">الانتظار: {waitingTime(order.createdAt)} دقيقه</p>
+                          <ul className='list-group list-group-flush'>
+                            {order.products.filter((pr) => pr.isDone === false).map((product, i) => {
+                              return (
+                                <li className='list-group-item d-flex justify-content-between align-items-center' key={i} style={product.isAdd ? { backgroundColor: 'red' } : {}}>
+                                  <div>
+                                    <p>{i + 1}- {product.name}</p>
+                                    <span> × {product.quantity}</span>
+                                  </div>
+                                  <div>{product.notes}</div>
+                                </li>
+                              )
+                            })}
+                          </ul>
+                          <div>
+                            {order.status === 'Preparing' ? <button className="btn btn-warning" onClick={() => { orderDone(order._id); updatecountofsales(order._id) }}>تم التنفيذ</button>
+                              : <button className="btn btn-success" onClick={() => orderInProgress(order._id)}>بدء التنفيذ</button>
+                            }
                           </div>
                         </div>
-                      )
-                    }
+                      </div>
+                    )
+                  }
 
-                  })
-                }
+                })
+              }
               )
             </div>
           )

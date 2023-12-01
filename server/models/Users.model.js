b45cb439c0f4bce7b4ac486c7c0f1a3bc58da8d6
beshoynady <bishoin2017@gmail.com>
2023-12-01 20:28:22 +0200

    userschema

--- server/models/Users.model.js
+++ server/models/Users.model.js
@@ -1,53 +1,60 @@
 const mongoose = require('mongoose');
 
 const userschema = new mongoose.Schema({
-    username :{
-        type : String,
-        unique : true,
-        require : [ true, 'required username' ],
-        trim : true,
-        minlength : 3,
-        maxlength : 100,
-    },
-    email : {
-        type : String,
-        unique : true,
-        maxlength : 100,
-        minlength : 10,
-        trim : true,
-    },
-    password : {
-        type : String,
-        trim : true,
-        require : [true , 'password required'],
-        maxlength : 200,
-        minlength : 3,
-    },
-    address:{
-        type:String,
+    username: {
+        type: String,
+        unique: true,
+        required: [true, 'Username is required'],
         trim: true,
-        minlength:3,
-        maxlength: 150,
-        
+        minlength: 3,
+        maxlength: 100,
     },
-    phone:{
-        type : String,
-        require : [true , 'phone required'],
-        trim : true,
+    email: {
+        type: String,
+        unique: true,
+        maxlength: 100,
+        minlength: 5,
+        trim: true,
+        validate: {
+            validator: function (v) {
+                // Simple email validation
+                return /^\S+@\S+\.\S+$/.test(v);
+            },
+            message: 'Please enter a valid email address',
+        },
+    },
+    password: {
+        type: String,
+        trim: true,
+        required: [true, 'Password is required'],
+        maxlength: 200,
+        minlength: 8,
+        select: false, // Hide password from query results
+    },
+    address: {
+        type: String,
+        trim: true,
+        minlength: 3,
+        maxlength: 150,
+        required: [true, 'Address is required'],
+    },
+    phone: {
+        type: String,
+        required: [true, 'Phone number is required'],
+        trim: true,
         length: 11,
     },
-
-    isVarified :{
-        type : Boolean,
-        default : false
-    },
-    isActive :{
+    isVerified: {
         type: Boolean,
-        default : true,
-        require : [true , 'isActive required'],
-    }
+        default: false,
+    },
+    isActive: {
+        type: Boolean,
+        default: true,
+        required: [true, 'isActive required'],
+    },
 },
-{timestamp: true}
+{ timestamps: true }
 );
 
 const Usermodel = mongoose.model('User', userschema);

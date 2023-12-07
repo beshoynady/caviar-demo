447854c5e25c3d94a95b13eb52bb98b5f6d5ca63
beshoynady <bishoin2017@gmail.com>
2023-12-07 20:23:55 +0200

    Header 0

--- client/src/screens/user.screen/component/header/Header.jsx
+++ client/src/screens/user.screen/component/header/Header.jsx
@@ -3,6 +3,7 @@ import { Link, useParams } from 'react-router-dom';
 import { useState, useRef } from 'react';
 import { detacontext } from '../../../../App';
 import LoginRegistr from '../auth/LoginRegistr';
+import Cart from '../cart/Cart';
 
 const Header = () => {
   const { id } = useParams();
@@ -17,51 +18,45 @@ const Header = () => {
   return (
     <detacontext.Consumer>
       {({ userLoginInfo, logout, ItemsInCart }) => (
-        <header className="bg-gradient">
-          <div className="container-lg">
-            <div className="row align-items-center">
-              <div className="col-md-6 col-8">
-                <div className="logo">
-                  <div className="mob-menu" onClick={toggleMobileMenu}>
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                  </div>
-                  <Link to="/" className="res-name">كافيار</Link>
-                </div>
+        <header>
+          <div>
+            <div>
+              <div onClick={toggleMobileMenu}>
+                <span></span>
+                <span></span>
+                <span></span>
               </div>
-              <div className="col-md-6 col-4 text-end">
-                <nav ref={navref} className="nav">
-                  <ul className="navigator">
-                    <li onClick={toggleMobileMenu}><Link to="/">الرئيسيه</Link></li>
-                    <li onClick={toggleMobileMenu}><Link to="#menu">قائمة الطعام</Link></li>
-                    <li onClick={toggleMobileMenu}><Link to="#offer">العروض</Link></li>
-                    <li onClick={toggleMobileMenu}><Link to="#location">موقعنا</Link></li>
-                    <li onClick={toggleMobileMenu}><Link to="#contact">تواصل معنا</Link></li>
-                  </ul>
-                </nav>
-                <div className="right-nav">
-                  {!id && (
-                    <>
-                      {userLoginInfo && userLoginInfo.userinfo ? (
-                        <div onClick={logout}> خروج
-                          <span>logout</span>
-                        </div>
-                      ) : (
-                        <div onClick={() => setopenlogin(!openlogin)}>دخول<span>
-                          login
-                        </span></div>
-                      )}
-                    </>
+              <Link to="/">كافيار</Link>
+            </div>
+            <nav ref={navref}>
+              <ul>
+                <li onClick={toggleMobileMenu}><Link to="/">الرئيسيه</Link></li>
+                <li onClick={toggleMobileMenu}><Link to="#menu">قائمة الطعام</Link></li>
+                <li onClick={toggleMobileMenu}><Link to="#offer">العروض</Link></li>
+                <li onClick={toggleMobileMenu}><Link to="#location">موقعنا</Link></li>
+                <li onClick={toggleMobileMenu}><Link to="#contact">تواصل معنا</Link></li>
+              </ul>
+            </nav>
+            <div>
+              {!id && (
+                <>
+                  {userLoginInfo && userLoginInfo.userinfo ? (
+                    <div onClick={logout}> خروج
+                      <span>logout</span>
+                    </div>
+                  ) : (
+                    <div onClick={() => setopenlogin(!openlogin)}>دخول<span>
+                      login
+                    </span></div>
                   )}
-                  <div onClick={() => setopencart(!opencart)}>
-                    <span>shopping_cart</span>
-                    <span>{ItemsInCart.length}</span>
-                  </div>
-                  <LoginRegistr openlogin={openlogin} />
-                  <Cart opencart={opencart} />
-                </div>
+                </>
+              )}
+              <div onClick={() => setopencart(!opencart)}>
+                <span>shopping_cart</span>
+                <span>{ItemsInCart.length}</span>
               </div>
+              <LoginRegistr openlogin={openlogin} />
+              <Cart opencart={opencart} />
             </div>
           </div>
         </header>
@@ -69,6 +64,7 @@ const Header = () => {
     </detacontext.Consumer>
   );
 };
+
 
 export default Header;
 

1aec207e7397a3b1f1dfb1bac4c9941f7b9d3cb1
beshoynady <bishoin2017@gmail.com>
2023-12-07 20:31:41 +0200

    Header 2

--- client/src/screens/user.screen/component/header/Header.jsx
+++ client/src/screens/user.screen/component/header/Header.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { useState, useRef } from 'react';
 import { detacontext } from '../../../../App';
 import LoginRegistr from '../auth/LoginRegistr';
 import Cart from '../cart/Cart';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
 
 const Header = () => {
   const { id } = useParams();
@@ -12,63 +13,71 @@ const Header = () => {
   const navref = useRef();
 
   const toggleMobileMenu = () => {
-    navref.current.classList.toggle("show");
+    navref.current.classList.toggle('show');
   };
 
+  const { userLoginInfo, logout, ItemsInCart } = useContext(detacontext); // Using useContext hook to access context
+
   return (
-    <detacontext.Consumer>
-      {({ userLoginInfo, logout, ItemsInCart }) => (
-        <header className="bg-gradient">
-          <div className="container">
-            <div className="row align-items-center">
-              <div className="col-lg-6 col-md-8 col-6">
-                <div className="logo">
-                  <div className="mob-menu" onClick={toggleMobileMenu}>
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                  </div>
-                  <Link to="/" className="res-name">كافيار</Link>
-                </div>
-              </div>
-              <div className="col-lg-6 col-md-4 col-6 text-end">
-                <nav ref={navref} className="nav" style={{ display: 'none' }}>
-                  <ul className="navigator">
-                    <li onClick={toggleMobileMenu}><Link to="/">الرئيسيه</Link></li>
-                    <li onClick={toggleMobileMenu}><Link to="#menu">قائمة الطعام</Link></li>
-                    <li onClick={toggleMobileMenu}><Link to="#offer">العروض</Link></li>
-                    <li onClick={toggleMobileMenu}><Link to="#location">موقعنا</Link></li>
-                    <li onClick={toggleMobileMenu}><Link to="#contact">تواصل معنا</Link></li>
-                  </ul>
-                </nav>
-                <div className="right-nav">
-                  {!id && (
-                    <>
-                      {userLoginInfo && userLoginInfo.userinfo ? (
-                        <div onClick={logout} className="cursor-pointer text-white"> خروج</div>
-                      ) : (
-                        <div onClick={() => setopenlogin(!openlogin)} className="cursor-pointer text-white">دخول</div>
-                      )}
-                    </>
+    <header>
+      <div className="container">
+        <div className="row align-items-center justify-content-between">
+          <div className="col-6 col-md-4">
+            <div onClick={toggleMobileMenu}>
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
+            <Link to="/">كافيار</Link>
+          </div>
+          <div className="col-6 col-md-8">
+            <nav ref={navref}>
+              <ul>
+                <li onClick={toggleMobileMenu}>
+                  <Link to="/">الرئيسيه</Link>
+                </li>
+                <li onClick={toggleMobileMenu}>
+                  <Link to="#menu">قائمة الطعام</Link>
+                </li>
+                <li onClick={toggleMobileMenu}>
+                  <Link to="#offer">العروض</Link>
+                </li>
+                <li onClick={toggleMobileMenu}>
+                  <Link to="#location">موقعنا</Link>
+                </li>
+                <li onClick={toggleMobileMenu}>
+                  <Link to="#contact">تواصل معنا</Link>
+                </li>
+              </ul>
+            </nav>
+            <div>
+              {!id && (
+                <>
+                  {userLoginInfo && userLoginInfo.userinfo ? (
+                    <div onClick={logout}>
+                      خروج
+                      <span>logout</span>
+                    </div>
+                  ) : (
+                    <div onClick={() => setopenlogin(!openlogin)}>
+                      دخول
+                      <span>login</span>
+                    </div>
                   )}
-                  <div onClick={() => setopencart(!opencart)} className="cursor-pointer text-white position-relative ms-3">
-                    <span className="me-2">shopping_cart</span>
-                    <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
-                      {ItemsInCart.length}
-                      <span className="visually-hidden">عناصر في السلة</span>
-                    </span>
-                  </div>
-                  {/* اضف LoginRegistr و Cart هنا */}
-                </div>
+                </>
+              )}
+              <div onClick={() => setopencart(!opencart)}>
+                <span>shopping_cart</span>
+                <span>{ItemsInCart.length}</span>
               </div>
+              <LoginRegistr openlogin={openlogin} />
+              <Cart opencart={opencart} />
             </div>
           </div>
-        </header>
-      )}
-    </detacontext.Consumer>
+        </div>
+      </div>
+    </header>
   );
 };
 
-
 export default Header;
-

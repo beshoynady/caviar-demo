0c954fbe50d741dcfb2505096d76d484c59215c4
beshoynady <bishoin2017@gmail.com>
2023-12-07 20:56:04 +0200

    Header 1 0

--- client/src/screens/user.screen/component/header/Header.jsx
+++ client/src/screens/user.screen/component/header/Header.jsx
@@ -1,83 +1,77 @@
-import React, { useContext } from 'react';
-import { Link, useParams } from 'react-router-dom';
-import { useState, useRef } from 'react';
-import { detacontext } from '../../../../App';
-import LoginRegistr from '../auth/LoginRegistr';
-import Cart from '../cart/Cart';
-import { Button } from 'react-bootstrap'; // Import Bootstrap Button component
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; // Assuming you're using FontAwesome for icons
-import { faSignInAlt, faSignOutAlt, faShoppingCart } from '@fortawesome/free-solid-svg-icons'; // Import required icons
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
 
 const Header = () => {
-  const { id } = useParams();
-  const [opencart, setopencart] = useState(false);
-  const [openlogin, setopenlogin] = useState(false);
-  const [showNav, setShowNav] = useState(false); // State to toggle mobile nav
-  const navref = useRef();
+  const [showNav, setShowNav] = useState(false);
 
   const toggleMobileMenu = () => {
     setShowNav(!showNav);
   };
 
-  const { userLoginInfo, logout, ItemsInCart } = useContext(detacontext); // Using useContext hook to access context
-
   return (
     <header>
-      <div className="container">
-        <div className="row align-items-center justify-content-between">
-          <div className="col-6 col-md-4">
-            <div onClick={toggleMobileMenu}>
-              <span></span>
-              <span></span>
-              <span></span>
-            </div>
-            <Link to="/">كافيار</Link>
-          </div>
-          <div className="col-6 col-md-8">
-            <nav ref={navref} className={showNav ? 'show' : 'hide'}>
-              <ul>
-                <li onClick={toggleMobileMenu}>
-                  <Link to="/">الرئيسيه</Link>
-                </li>
-                <li onClick={toggleMobileMenu}>
-                  <Link to="#menu">قائمة الطعام</Link>
-                </li>
-                <li onClick={toggleMobileMenu}>
-                  <Link to="#offer">العروض</Link>
-                </li>
-                <li onClick={toggleMobileMenu}>
-                  <Link to="#location">موقعنا</Link>
-                </li>
-                <li onClick={toggleMobileMenu}>
-                  <Link to="#contact">تواصل معنا</Link>
-                </li>
-              </ul>
-            </nav>
-            <div>
-              {!id && (
-                <>
-                  {userLoginInfo && userLoginInfo.userinfo ? (
-                    <Button variant="primary" onClick={logout}>
-                      خروج <FontAwesomeIcon icon={faSignOutAlt} />
-                    </Button>
-                  ) : (
-                    <Button variant="primary" onClick={() => setopenlogin(!openlogin)}>
-                      دخول <FontAwesomeIcon icon={faSignInAlt} />
-                    </Button>
-                  )}
-                </>
-              )}
-              <Button variant="primary" onClick={() => setopencart(!opencart)}>
-                <FontAwesomeIcon icon={faShoppingCart} />{' '}
-                <span>{ItemsInCart.length}</span>
-              </Button>
-              <LoginRegistr openlogin={openlogin} />
-              <Cart opencart={opencart} />
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container">
+          <Link className="navbar-brand" to="/">
+            كافيار
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            onClick={toggleMobileMenu}
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div
+            className={`collapse navbar-collapse ${
+              showNav ? 'show' : ''
+            }`}
+          >
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link className="nav-link" to="/">
+                  الرئيسية
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="#menu">
+                  قائمة الطعام
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="#offer">
+                  العروض
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="#location">
+                  موقعنا
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="#contact">
+                  تواصل معنا
+                </Link>
+              </li>
+            </ul>
+            <div className="d-flex align-items-center">
+              <Link className="btn btn-primary me-3" to="/login">
+                تسجيل الدخول
+              </Link>
+              <Link className="btn btn-outline-primary me-3" to="/signup">
+                إنشاء حساب
+              </Link>
+              <Link className="btn btn-outline-primary position-relative" to="/cart">
+                <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                  5 {/* Replace with dynamic cart count */}
+                </span>
+                <i className="bi bi-cart"></i>
+              </Link>
             </div>
           </div>
         </div>
-      </div>
+      </nav>
     </header>
   );
 };

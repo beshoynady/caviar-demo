55930c1df5444d089656c91fc1e0a09377c76a35
beshoynady <bishoin2017@gmail.com>
2023-12-07 20:29:45 +0200

    Header 1

--- client/src/screens/user.screen/component/header/Header.jsx
+++ client/src/screens/user.screen/component/header/Header.jsx
@@ -18,45 +18,49 @@ const Header = () => {
   return (
     <detacontext.Consumer>
       {({ userLoginInfo, logout, ItemsInCart }) => (
-        <header>
-          <div>
-            <div>
-              <div onClick={toggleMobileMenu}>
-                <span></span>
-                <span></span>
-                <span></span>
+        <header className="bg-gradient">
+          <div className="container">
+            <div className="row align-items-center">
+              <div className="col-lg-6 col-md-8 col-6">
+                <div className="logo">
+                  <div className="mob-menu" onClick={toggleMobileMenu}>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                  </div>
+                  <Link to="/" className="res-name">كافيار</Link>
+                </div>
               </div>
-              <Link to="/">كافيار</Link>
-            </div>
-            <nav ref={navref}>
-              <ul>
-                <li onClick={toggleMobileMenu}><Link to="/">الرئيسيه</Link></li>
-                <li onClick={toggleMobileMenu}><Link to="#menu">قائمة الطعام</Link></li>
-                <li onClick={toggleMobileMenu}><Link to="#offer">العروض</Link></li>
-                <li onClick={toggleMobileMenu}><Link to="#location">موقعنا</Link></li>
-                <li onClick={toggleMobileMenu}><Link to="#contact">تواصل معنا</Link></li>
-              </ul>
-            </nav>
-            <div>
-              {!id && (
-                <>
-                  {userLoginInfo && userLoginInfo.userinfo ? (
-                    <div onClick={logout}> خروج
-                      <span>logout</span>
-                    </div>
-                  ) : (
-                    <div onClick={() => setopenlogin(!openlogin)}>دخول<span>
-                      login
-                    </span></div>
+              <div className="col-lg-6 col-md-4 col-6 text-end">
+                <nav ref={navref} className="nav" style={{ display: 'none' }}>
+                  <ul className="navigator">
+                    <li onClick={toggleMobileMenu}><Link to="/">الرئيسيه</Link></li>
+                    <li onClick={toggleMobileMenu}><Link to="#menu">قائمة الطعام</Link></li>
+                    <li onClick={toggleMobileMenu}><Link to="#offer">العروض</Link></li>
+                    <li onClick={toggleMobileMenu}><Link to="#location">موقعنا</Link></li>
+                    <li onClick={toggleMobileMenu}><Link to="#contact">تواصل معنا</Link></li>
+                  </ul>
+                </nav>
+                <div className="right-nav">
+                  {!id && (
+                    <>
+                      {userLoginInfo && userLoginInfo.userinfo ? (
+                        <div onClick={logout} className="cursor-pointer text-white"> خروج</div>
+                      ) : (
+                        <div onClick={() => setopenlogin(!openlogin)} className="cursor-pointer text-white">دخول</div>
+                      )}
+                    </>
                   )}
-                </>
-              )}
-              <div onClick={() => setopencart(!opencart)}>
-                <span>shopping_cart</span>
-                <span>{ItemsInCart.length}</span>
+                  <div onClick={() => setopencart(!opencart)} className="cursor-pointer text-white position-relative ms-3">
+                    <span className="me-2">shopping_cart</span>
+                    <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                      {ItemsInCart.length}
+                      <span className="visually-hidden">عناصر في السلة</span>
+                    </span>
+                  </div>
+                  {/* اضف LoginRegistr و Cart هنا */}
+                </div>
               </div>
-              <LoginRegistr openlogin={openlogin} />
-              <Cart opencart={opencart} />
             </div>
           </div>
         </header>

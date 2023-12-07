61e0fcca7ad813ed0c4788657d06ed6436ed1f76
beshoynady <bishoin2017@gmail.com>
2023-12-07 21:04:24 +0200

    Header 1 2

--- client/src/screens/user.screen/component/header/Header.jsx
+++ client/src/screens/user.screen/component/header/Header.jsx
@@ -1,9 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
+import { detacontext } from '../../../../App';
 
 const Header = () => {
   const [showNav, setShowNav] = useState(false);
+  const { userLoginInfo, logout, ItemsInCart } = useContext(detacontext);
 
   const toggleMobileMenu = () => {
     setShowNav(!showNav);
@@ -56,17 +58,20 @@ const Header = () => {
               </li>
             </ul>
             <div className="d-flex align-items-center">
-              <Link className="btn btn-primary me-3" to="/login">
-                تسجيل الدخول
-              </Link>
-              <Link className="btn btn-outline-primary me-3" to="/signup">
-                إنشاء حساب
-              </Link>
+              {userLoginInfo && userLoginInfo.userinfo ? (
+                <Link className="btn btn-primary me-3" onClick={logout}>
+                  تسجيل الخروج
+                </Link>
+              ) : (
+                <Link className="btn btn-primary me-3" to="/login">
+                  تسجيل الدخول
+                </Link>
+              )}
               <Link className="btn btn-outline-primary position-relative" to="/cart">
                 <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
-                  5 {/* Replace with dynamic cart count */}
+                  {ItemsInCart.length}
                 </span>
-                <i className="bi bi-cart"></i>
+                سلة التسوق
               </Link>
             </div>
           </div>
@@ -77,3 +82,75 @@ const Header = () => {
 };
 
 export default Header;
+
+
+
+
+// import React, { useRef, useState } from 'react';
+// import { Link, useParams } from 'react-router-dom';
+// import { detacontext } from '../../../../App';
+// import './Header.css';
+// import Cart from '../cart/Cart';
+// import LoginRegistr from '../auth/LoginRegistr';
+
+// const Header = () => {
+//   const { id } = useParams();
+//   const [opencart, setopencart] = useState(false);
+//   const [openlogin, setopenlogin] = useState(false);
+//   const navref = useRef();
+
+//   const toggleMobileMenu = () => {
+//     navref.current.classList.toggle("show");
+//   };
+
+//   return (
+//     <detacontext.Consumer>
+//       {({ userLoginInfo, logout, ItemsInCart }) => (
+//         <header className='header-client'>
+//           <div className="container-lg">
+//             <div className='logo'>
+//               <div className="mob-menu" onClick={toggleMobileMenu}>
+//                 <span id='line-1'></span>
+//                 <span id='line-2'></span>
+//                 <span id='line-3'></span>
+//               </div>
+//               <Link to="/" className='res-name'>كافيار</Link>
+//             </div>
+//             <nav ref={navref} className='nav'>
+//               <ul className='navigator'>
+//                 <li onClick={toggleMobileMenu}><Link to="/">الرئيسيه</Link></li>
+//                 <li onClick={toggleMobileMenu}><Link to="#menu">قائمة الطعام</Link></li>
+//                 <li onClick={toggleMobileMenu}><Link to="#offer">العروض</Link></li>
+//                 <li onClick={toggleMobileMenu}><Link to="#location">موقعنا</Link></li>
+//                 <li onClick={toggleMobileMenu}><Link to="#contact">تواصل معنا</Link></li>
+//               </ul>
+//             </nav>
+//             <div className='right-nav'>
+//               {!id && (
+//                 <>
+//                   {userLoginInfo && userLoginInfo.userinfo ? (
+//                     <div className="nav-logout" onClick={logout}> خروج
+//                       <span className="material-symbols-outlined">logout</span>
+//                     </div>
+//                   ) : (
+//                     <div className='nav-login' onClick={() => setopenlogin(!openlogin)}>دخول<span className="material-symbols-outlined">
+//                       login
+//                     </span></div>
+//                   )}
+//                 </>
+//               )}
+//               <div className='cart-icon' onClick={() => setopencart(!opencart)}>
+//                 <span className="material-symbols-rounded shopping_cart">shopping_cart</span>
+//                 <span className='cartcounter'>{ItemsInCart.length}</span>
+//               </div>
+//               <LoginRegistr openlogin={openlogin} />
+//               <Cart opencart={opencart} />
+//             </div>
+//           </div>
+//         </header>
+//       )}
+//     </detacontext.Consumer>
+//   );
+// };
+
+// export default Header;

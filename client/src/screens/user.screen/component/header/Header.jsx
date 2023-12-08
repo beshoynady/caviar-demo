6d221c71eac1c14d0d781126a892635b3b8879a5
beshoynady <bishoin2017@gmail.com>
2023-12-08 10:01:53 +0200

    Header

--- client/src/screens/user.screen/component/header/Header.jsx
+++ client/src/screens/user.screen/component/header/Header.jsx
@@ -1,7 +1,10 @@
+import './Header.css';
+import Cart from '../cart/Cart';
+import LoginRegistr from '../auth/LoginRegistr';
 import React, { useRef, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { detacontext } from '../../../../App';
-import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Header = () => {
   const { id } = useParams();
@@ -16,37 +19,37 @@ const Header = () => {
   return (
     <detacontext.Consumer>
       {({ userLoginInfo, logout, ItemsInCart }) => (
-        <header className='header-client'>
+        <header className='bg-primary py-3'>
           <div className="container-lg d-flex justify-content-between align-items-center">
             <div className='logo d-flex align-items-center'>
-              <div className="mob-menu" onClick={toggleMobileMenu}>
+              <div className="mob-menu d-lg-none" onClick={toggleMobileMenu}>
                 <span className="bg-dark rounded p-1"></span>
                 <span className="bg-dark rounded p-1 mt-1"></span>
                 <span className="bg-dark rounded p-1 mt-1"></span>
               </div>
-              <Link to="/" className='res-name text-decoration-none'>كافيار</Link>
+              <Link to="/" className='text-gold text-decoration-none fs-3 fw-bold'>كافيار</Link>
             </div>
-            <nav ref={navref} className='nav'>
-              <ul className='navigator list-unstyled d-flex justify-content-between m-0'>
-                <li onClick={toggleMobileMenu}><Link to="/">الرئيسيه</Link></li>
-                <li onClick={toggleMobileMenu}><Link to="#menu">قائمة الطعام</Link></li>
-                <li onClick={toggleMobileMenu}><Link to="#offer">العروض</Link></li>
-                <li onClick={toggleMobileMenu}><Link to="#location">موقعنا</Link></li>
-                <li onClick={toggleMobileMenu}><Link to="#contact">تواصل معنا</Link></li>
+            <nav ref={navref} className='d-lg-block'>
+              <ul className='list-unstyled d-flex justify-content-between m-0'>
+                <li onClick={toggleMobileMenu}><Link to="/" className="text-white text-decoration-none">الرئيسيه</Link></li>
+                <li onClick={toggleMobileMenu}><Link to="#menu" className="text-white text-decoration-none">قائمة الطعام</Link></li>
+                <li onClick={toggleMobileMenu}><Link to="#offer" className="text-white text-decoration-none">العروض</Link></li>
+                <li onClick={toggleMobileMenu}><Link to="#location" className="text-white text-decoration-none">موقعنا</Link></li>
+                <li onClick={toggleMobileMenu}><Link to="#contact" className="text-white text-decoration-none">تواصل معنا</Link></li>
               </ul>
             </nav>
             <div className='right-nav d-flex align-items-center'>
               {!id && (
                 <>
                   {userLoginInfo && userLoginInfo.userinfo ? (
-                    <div className="nav-logout bg-danger text-white px-2 py-1 rounded cursor-pointer" onClick={logout}>
+                    <div className="bg-danger text-white px-2 py-1 rounded cursor-pointer" onClick={logout}>
                       خروج
-                      <span className="material-symbols-outlined text-white">logout</span>
+                      <span className="text-white">logout</span>
                     </div>
                   ) : (
-                    <div className='nav-login bg-light text-dark px-2 py-1 rounded cursor-pointer' onClick={() => setopenlogin(!openlogin)}>
+                    <div className='bg-light text-dark px-2 py-1 rounded cursor-pointer' onClick={() => setopenlogin(!openlogin)}>
                       دخول
-                      <span className="material-symbols-outlined">
+                      <span>
                         login
                       </span>
                     </div>
@@ -54,8 +57,8 @@ const Header = () => {
                 </>
               )}
               <div className='cart-icon position-relative' onClick={() => setopencart(!opencart)}>
-                <span className="material-symbols-rounded shopping_cart text-white">shopping_cart</span>
-                <span className='cartcounter bg-danger text-white rounded-circle d-flex align-items-center justify-content-center'>{ItemsInCart.length}</span>
+                <span className="text-white fs-4">shopping_cart</span>
+                <span className='bg-danger text-white rounded-circle d-flex align-items-center justify-content-center fw-bold'>{ItemsInCart.length}</span>
               </div>
               {/* Include LoginRegistr and Cart components here */}
             </div>
@@ -67,6 +70,7 @@ const Header = () => {
 };
 
 export default Header;
+
 
 
 // import React, { useRef, useState } from 'react';

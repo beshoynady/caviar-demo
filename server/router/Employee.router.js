368616c70d3c2ed22258f7cee61daea4f7f3ca02
beshoynady <bishoin2017@gmail.com>
2023-12-24 21:22:01 +0200

    Employee router

--- server/router/Employee.router.js
+++ server/router/Employee.router.js
@@ -1,22 +1,22 @@
 const express = require("express");
 const router = express.Router();
-const employeesController = require('../controllers/Employee.controller.js');
+const { createEmployee, getoneEmployee, loginEmployee,updateOrAddPayrollForMonth,paidPayrollForMonth, getallEmployees, updateEmployee, deleteEmployee } = require('../controllers/Employee.controller.js');
 
 router.route('/')
-    .post(employeesController.createEmployee)
-    .get(employeesController.getallEmployees);
+    .post(createEmployee)
+    .get(getallEmployees);
 
 router.route('/:employeeId')
-    .get(employeesController.getoneEmployee)
-    .put(employeesController.updateEmployee)
-    .delete(employeesController.deleteEmployee);
+    .get(getoneEmployee)
+    .put(updateEmployee)
+    .delete(deleteEmployee);
 
-router.route('/login').post(employeesController.loginEmployee);
+router.route('/login').post(loginEmployee);
 
 router.route('/payroll/:employeeId')
-    .put(employeesController.paidPayrollForMonth);
+    .put(paidPayrollForMonth);
 
 // router.route('/paid/:employeeId')
-//     .put(employeesController.paidPayrollForMonth);
+//     .put(paidPayrollForMonth);
 
 module.exports = router;
